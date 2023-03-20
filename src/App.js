c79b222743e7fb8354317a399b7c67e28d8022b4
jzlbornoz <javieraap10@gmail.com>
2023-03-20 16:31:07 -0400

    notification

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import addNotification from 'react-push-notification';
+import logo from './logo.svg'
 
 function App() {
+  const toNotificate = () => {
+    addNotification({
+      title: "Tarea hecha",
+      message: "Le toca a",
+      duration: 4000,
+      icon: logo,
+      native: true
+    })
+  }
+
   return (
     <div >
       <header>
         Notification App | Pruebas Javier Albornoz
       </header>
-      <button>
+      <button type='button' onClick={toNotificate()}>
         Done!
       </button>
     </div>
