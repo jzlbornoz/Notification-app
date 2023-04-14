17a5cc3ba5c55deeba9ec2641e87b2001b4adc29
jzlbornoz <javieraap10@gmail.com>
2023-04-14 09:59:16 -0400

    [CLOUDINARY]

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg'
 import { getAuth, signInAnonymously } from 'firebase/auth'
 import { getToken, onMessage } from 'firebase/messaging'
 import { Message } from './firebase';
@@ -35,19 +34,57 @@ function App() {
     })
   }, []);
 
+
+  // image handler
+  const [image, setImage] = useState("");
+
+  const [uploadedImage, setUploadedImage] = useState("");
+
+  const submitImage = () => {
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "appDemo");
+    data.append("cloud_name", "dsdrcuojk");
+
+    fetch("https://api.cloudinary.com/v1_1/dsdrcuojk/image/upload", {
+      method: "post",
+      body: data
+    })
+      .then((res) => res.json())
+      .then((dta) => {
+        setUploadedImage(dta);
+        console.log("DATA", dta);
+        //console.log("IMAGE", uploadedImage);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  };
+
+  console.log("IIMAGE URL" , uploadedImage.url);
+  // --
   return (
     <div >
       <header>
-        Notification App | Pruebas Javier Albornoz
+        Notification App / Image Handler | Pruebas Javier Albornoz
       </header>
-      <ToastContainer />
-      <p>Token: {tokenCopy}</p>
-      <button type='button' onClick={logIn}>
-        logIn
-      </button>
-      <button type='button' onClick={activateMessage}>
-        Token
-      </button>
+      <>
+        <ToastContainer />
+        <p>Token: {tokenCopy}</p>
+        <button type='button' onClick={logIn}>
+          logIn
+        </button>
+        <button type='button' onClick={activateMessage}>
+          Token
+        </button>
+      </>
+      <>
+        <div>
+          <input type='file' onChange={(e) => setImage(e.target.files[0])} />
+          <button onClick={submitImage}>upload</button>
+        </div>
+      <img src={uploadedImage.url} alt='s' /> 
+      </>
     </div>
   );
 }
